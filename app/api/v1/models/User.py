8ba615d24ae333c030fd28f1a25ad062c9ba5788
Morris Warachi <morriswarachi0@gmail.com>
2018-10-24 21:57:57 +0300

    [Chore #161431131] create User model

---
+++ app/api/v1/models/User.py
@@ -0,0 +1,7 @@
+"""User model class"""
+
+class user:
+    """User class contains user constructor and authentication methods"""
+    def __init__(self):
+        """Initialize User Object with an email and password"""
+        pass
